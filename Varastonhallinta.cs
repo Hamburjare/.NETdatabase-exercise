39490010855cc0148c46ccc1d4121b0f3407bbf6
Hamburjare <93974508+Hamburjare@users.noreply.github.com>
2022-11-08 12:12:26 +0200

    Database connection fixed

--- Varastonhallinta.cs
+++ Varastonhallinta.cs
@@ -7,13 +7,15 @@ namespace Varastonhallinta
         public DbSet<Varasto>? Tuotteet { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string connection = "Data Source=.;" +
-            "Initial Catalog=Varastonhallinta;" +
-            "Integrated Security=true;" + 
-            //"User Id = sa; " + "Password = salasana; " 
-            "MultipleActiveResultSets=true;";
+            // string connection = "Data Source=.;" +
+            // "Initial Catalog=Varastonhallinta;" +
+            // "Integrated Security=true;" + 
+            // "User Id = sa; " + "Password = salasana; " +
+            // "MultipleActiveResultSets=true;";
+            string connection = $"Data Source=.; Initial Catalog=Varastonhallinta; Integrated Security=FALSE; User Id = sa; Password = pX45@x65f4LX?tjb;";
             // Tässä otetaan yhteys tietokantaan
             optionsBuilder.UseSqlServer(connection);
+
         }
     }
 }
